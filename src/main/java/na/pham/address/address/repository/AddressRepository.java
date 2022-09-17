4a32363a100be822dda42816988de6ec448b46e6
AnhPham2202 <ngocanhpham220299@gmail.com>
2022-09-17 12:18:42 +0700

    basic api and config eureka client

---
+++ src/main/java/na/pham/address/address/repository/AddressRepository.java
@@ -0,0 +1,10 @@
+package na.pham.address.address.repository;
+
+import na.pham.address.address.entity.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address, Long> {
+
+}
